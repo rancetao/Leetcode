b82d8b4b1dda7b426447437e6d31d41b8cf181a5
rancetao <tao.rance@gmail.com>
2013-10-14 17:29:15 -0700

    first try

---
+++ Leetcode/src/com/ran/leetcode/fdsd.java
@@ -0,0 +1,15 @@
+package com.ran.leetcode;
+
+
+public class fdsd {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+
+    }
+
+}
