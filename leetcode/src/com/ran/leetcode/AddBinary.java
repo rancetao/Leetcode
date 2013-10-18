bcbb2d1df6d569dcadae4630e4e4ba6e47bd0d44
rancetao <tao.rance@gmail.com>
2013-10-17 17:53:28 -0700

    finally I know how to push and fetch

---
+++ leetcode/src/com/ran/leetcode/AddBinary.java
@@ -0,0 +1,46 @@
+package com.ran.leetcode;
+
+/**
+ * 
+ * Given two binary strings, return their sum (also a binary string).
+ * 
+ * For example, a = "11" b = "1" Return "100".
+ * 
+ * @author taor
+ * @since Oct 17, 2013
+ */
+public class AddBinary {
+
+    public String addBinary(String a, String b) {
+
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        int carry = 0;
+        String sumStr = "";
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        while (i >= 0 || j >= 0) {
+            int bitSum = 0;
+            if (i >= 0 && j >= 0) {
+                bitSum = Integer.parseInt(a.substring(i, i + 1)) + Integer.parseInt(b.substring(j, j + 1)) + carry;
+                i--;
+                j--;
+            }
+            else if (i >= 0) {
+                bitSum = Integer.parseInt(a.substring(i, i + 1)) + carry;
+                i--;
+            }
+            else {
+                bitSum = Integer.parseInt(b.substring(j, j + 1)) + carry;
+                j--;
+            }
+            carry = bitSum / 2;
+            int bit = bitSum % 2;
+            sumStr = bit + sumStr;
+        }
+        if (carry == 1) {
+            sumStr = carry + sumStr;
+        }
+        return sumStr;
+    }
+
+}
---
+++ leetcode/src/com/ran/leetcode/SearchInRotatedSortedArray.java
@@ -0,0 +1,52 @@
+package com.ran.leetcode;
+
+/**
+ * Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+ * 
+ * (i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+ * 
+ * You are given a target value to search. If found in the array return its index, otherwise return -1.
+ * 
+ * You may assume no duplicate exists in the array.
+ * 
+ * @author taor
+ * @since Oct 17, 2013
+ */
+public class SearchInRotatedSortedArray {
+
+    public int search(int[] A, int target) {
+
+        int begin = 0;
+        int end = A.length - 1;
+
+        while (begin <= end) {
+            int mid = begin + (end - begin) / 2;
+            if (A[mid] == target)
+                return mid;
+            if (A[begin] <= A[mid]) {
+                if (A[begin] <= target && target <= A[mid]) {
+                    end = mid - 1;
+                }
+                else {
+                    begin = mid + 1;
+                }
+            }
+            else {
+                if (A[mid] <= target && target <= A[end]) {
+                    begin = mid + 1;
+                }
+                else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+        // TODO Auto-generated method stub
+
+    }
+
+}
