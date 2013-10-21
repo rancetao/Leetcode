b975d7f83b4d7af3d1df3095262a9554e7b85e0c
rancetao <tao.rance@gmail.com>
2013-10-21 16:05:34 -0700

    new solution

---
+++ leetcode/src/com/ran/leetcode/IntegerToRoman.java
@@ -0,0 +1,33 @@
+package com.ran.leetcode;
+
+/**
+ * Given an integer, convert it to a roman numeral.
+ * 
+ * Input is guaranteed to be within the range from 1 to 3999.
+ * 
+ * 
+ * Symbol Value I 1 V 5 X 10 L 50 C 100 D 500 M 1,000
+ * 
+ * @author taor
+ * @since Oct 18, 2013
+ */
+public class IntegerToRoman {
+
+    public String intToRoman(int num) {
+
+        int[] nums = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder res = new StringBuilder();
+        int i = 0;
+        while (num > 0) {
+            int times = num / nums[i];
+            num -= nums[i] * times;
+            while(times > 0){
+                res.append(symbols[i]);
+                --times;
+            }
+            ++i;
+        }
+        return res.toString();
+    }
+}
