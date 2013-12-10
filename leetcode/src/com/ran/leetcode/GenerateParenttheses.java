3f153a0c16de1a3b683f6475725f66dfd7c497ca
rancetao <tao.rance@gmail.com>
2013-12-09 17:39:13 -0800

    init the class

---
+++ leetcode/src/com/ran/leetcode/GenerateParenttheses.java
@@ -0,0 +1,24 @@
+package com.ran.leetcode;
+
+import java.util.ArrayList;
+
+/**
+ * Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+ * 
+ * For example, given n = 3, a solution set is:
+ * 
+ * "((()))", "(()())", "(())()", "()(())", "()()()"
+ * 
+ * @author taor
+ * @date Dec 9, 2013
+ */
+
+public class GenerateParenttheses {
+
+    public ArrayList<String> generateParenthesis(int n) {
+        
+        
+        return null;
+
+    }
+}
