9a2b94f51998f6481e7b5d7056cfadaec36f05a6
rancetao <tao.rance@gmail.com>
2014-03-03 18:10:08 -0800

    haven't done yet. has issues

--- leetcode/src/com/ran/leetcode/GenerateParenttheses.java
+++ leetcode/src/com/ran/leetcode/GenerateParenttheses.java
@@ -16,9 +16,42 @@ import java.util.ArrayList;
 public class GenerateParenttheses {
 
     public ArrayList<String> generateParenthesis(int n) {
-        
-        
-        return null;
 
+        ArrayList<String> arrList = new ArrayList<String>();
+        if (n == 0) {
+            arrList.add("");
+            return arrList;
+        }
+
+        if (n == 1) {
+            arrList.add("()");
+            return arrList;
+        }
+
+        arrList = generateParenthesis(n - 1);
+        int tempSize = arrList.size();
+        ArrayList<String> tempArrList = new ArrayList<String>();
+        for (int j = 0; j < tempSize; j++) {
+            String tempStr = arrList.get(j);
+            StringBuffer sb = new StringBuffer(tempStr);
+            int size = sb.length();
+            tempArrList = new ArrayList<String>();
+            for (int i = 0; i < size; ++i) {
+                String str = sb.insert(i, "()").toString();
+                if (!arrList.contains(str)) {
+                    tempArrList.add(str);
+                }
+            }
+        }
+        arrList.addAll(tempArrList);
+        return arrList;
+    }
+
+    public static void main(String[] args) {
+        GenerateParenttheses gp = new GenerateParenttheses();
+        ArrayList<String> arrList = gp.generateParenthesis(3);
+        for (String s : arrList) {
+            System.out.println(s);
+        }
     }
 }
