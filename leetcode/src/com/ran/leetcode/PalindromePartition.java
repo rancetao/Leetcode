5888ef211f3eae5f503656574036d98a67ac693f
rancetao <tao.rance@gmail.com>
2014-03-05 18:06:35 -0800

    haven't done yet

--- leetcode/src/com/ran/leetcode/PalindromePartition.java
+++ leetcode/src/com/ran/leetcode/PalindromePartition.java
@@ -18,7 +18,40 @@ public class PalindromePartition {
 
     public ArrayList<ArrayList<String>> partition(String s) {
 
-        return null;
+        ArrayList<ArrayList<String>> resultArrLists = new ArrayList<ArrayList<String>>();
+
+        if (s == null)
+            return null;
+        int length = s.length();
+        if (length == 0) {
+            return resultArrLists;
+        }
+        if (length == 1) {
+            ArrayList<String> arrLista = new ArrayList<String>();
+            arrLista.add(s);
+            resultArrLists.add(arrLista);
+            return resultArrLists;
+        }
+
+        String lastChar = s.substring(length - 1);
+        ArrayList<ArrayList<String>> arrLists = partition(s.substring(0, length - 1));
+        int arrListsSize = arrLists.size();
+
+        for (int i = 0; i < arrListsSize; ++i) {
+            ArrayList<String> arrayList = arrLists.get(i);
+
+            int arrayListSize = arrayList.size();
+            for (int j = 0; j < arrayListSize; ++j) {
+
+                String str = arrayList.get(j).concat(lastChar);
+                if (isPalindrome(str)) {
+                    arrayList.set(j, str);
+                    arrLists.set(i, arrayList);
+                }
+            }
+
+        }
+        return arrLists;
     }
 
     public Boolean isPalindrome(String s) {
@@ -33,13 +66,18 @@ public class PalindromePartition {
             if (s.charAt(begin) != s.charAt(end)) {
 
                 return false;
-            }
-            else {
+            } else {
 
                 ++begin;
                 --end;
             }
         }
         return true;
+    }
+
+    public static void main(String args[]) {
+        PalindromePartition pp = new PalindromePartition();
+        ArrayList<ArrayList<String>> resultArrLists = pp.partition("aab");
+        System.out.println("done");
     }
 }
