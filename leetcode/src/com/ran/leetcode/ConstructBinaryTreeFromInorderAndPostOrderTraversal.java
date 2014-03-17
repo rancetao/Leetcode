2f2264a18be55645d3afc35c46b7a6f77e296bf6
rancetao <tao.rance@gmail.com>
2014-03-17 15:35:10 -0700

    working on that

---
+++ leetcode/src/com/ran/leetcode/ConstructBinaryTreeFromInorderAndPostOrderTraversal.java
@@ -0,0 +1,13 @@
+package com.ran.leetcode;
+
+/**
+ * Given inorder and postorder traversal of a tree, construct the binary tree.
+ * 
+ * Note: You may assume that duplicates do not exist in the tree.
+ * 
+ * @author taor
+ * @date Mar 17, 2014
+ */
+public class ConstructBinaryTreeFromInorderAndPostOrderTraversal {
+
+}
