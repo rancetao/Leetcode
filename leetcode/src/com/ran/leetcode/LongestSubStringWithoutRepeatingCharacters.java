a851c0bc6a06bc73e6be30bd664aa879636ebb68
RanceTao <tao.rance@gmail.com>
2014-07-28 00:11:28 -0700

    digest

--- leetcode/src/com/ran/leetcode/LongestSubStringWithoutRepeatingCharacters.java
+++ leetcode/src/com/ran/leetcode/LongestSubStringWithoutRepeatingCharacters.java
@@ -1,12 +1,66 @@
 package com.ran.leetcode;
 
+import java.util.HashMap;
+
 /**
- * Given a string, find the length of the longest substring without repeating characters. 
- * For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. 
- * For "bbbbb" the longest substring is "b", with the length of 1.
+ * Given a string, find the length of the longest substring without repeating characters. For
+ * example, the longest substring without repeating letters for "abcabcbb" is "abc", which the
+ * length is 3. For "bbbbb" the longest substring is "b", with the length of 1.
+ * 
  * @author RanceTao
- *
+ * 
  */
 public class LongestSubStringWithoutRepeatingCharacters {
 
+    public int lengthOfLongestSubstring(String s) {
+        boolean[] flag = new boolean[256];
+
+        int result = 0;
+        int j = 0;
+        char[] arr = s.toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            char c = arr[i];
+            if (flag[c]) {
+                result = Math.max(result, i - j);
+                for (int k = j; k < i; k++) {
+                    if (arr[k] == c) {
+                        j = k + 1;
+                        break;
+                    }
+                    flag[arr[k]] = false;
+                }
+            } else {
+                flag[c] = true;
+            }
+        }
+
+        result = Math.max(arr.length - j, result);
+
+        return result;
+    }
+
+    public static int lengthOfLongestSubstring2(String s) {
+
+        char[] arr = s.toCharArray();
+        int pre = 0;
+
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (!map.containsKey(arr[i])) {
+                map.put(arr[i], i);
+            } else {
+                pre = pre > map.size() ? pre : map.size();
+                i = map.get(arr[i]);
+                map.clear();
+            }
+        }
+
+        return Math.max(pre, map.size());
+    }
+
+    public static void main(String args[]) {
+
+    }
 }
--- leetcode/src/com/ran/leetcode/PermutationSequence.java
+++ leetcode/src/com/ran/leetcode/PermutationSequence.java
@@ -1,70 +1,61 @@
 package com.ran.leetcode;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * The set [1,2,3,…,n] contains a total of n! unique permutations.
-
-	By listing and labeling all of the permutations in order,
-	We get the following sequence (ie, for n = 3):
-	
-	"123"
-	"132"
-	"213"
-	"231"
-	"312"
-	"321"
-	Given n and k, return the kth permutation sequence.
-	
-	Note: Given n will be between 1 and 9 inclusive.
+ * 
+ * By listing and labeling all of the permutations in order, We get the following sequence (ie, for
+ * n = 3):
+ * 
+ * "123" "132" "213" "231" "312" "321" Given n and k, return the kth permutation sequence.
+ * 
+ * Note: Given n will be between 1 and 9 inclusive.
+ * 
  * @author RanceTao
- *
+ * 
  */
 
 public class PermutationSequence {
 
-	public ArrayList<String> getPermutations(int n){
-		
-		ArrayList<String> result = new ArrayList<String>();
-		ArrayList<String> tempResult = new ArrayList<String>();
-		
-		if(n < 1)
-			return null;
-		if(n == 1){
-			result.add("1");
-			return result;
-		}
-		
-		ArrayList<String> lastResult = getPermutations(n-1);
-		for(int i = 0; i < lastResult.size(); i++){
-			String temp = lastResult.get(i);
-			for(int j = n-1; j >0; j--){
-				
-				String newItem = temp.substring(0, j) + String.valueOf(n) + temp.substring(j);
-				result.add(newItem);
-				
-			}
-			tempResult.add(String.valueOf(n) +temp);
-		}
-		
-		result.addAll(tempResult);
-		
-		return result;
-		
-		StringBuilder sb = new StringBuilder("ss");
-		sb.re
-		
-	}
-	
-	public String getPermutation(int n, int k) {
-		ArrayList<String> list = getPermutations(n);		
-		System.out.println(list.get(k-1));
-		return list.get(k-1);
+    public ArrayList<String> getPermutations(int n) {
+
+        ArrayList<String> result = new ArrayList<String>();
+        ArrayList<String> tempResult = new ArrayList<String>();
+
+        if (n < 1)
+            return null;
+        if (n == 1) {
+            result.add("1");
+            return result;
+        }
+
+        ArrayList<String> lastResult = getPermutations(n - 1);
+        for (int i = 0; i < lastResult.size(); i++) {
+            String temp = lastResult.get(i);
+            for (int j = n - 1; j > 0; j--) {
+
+                String newItem = temp.substring(0, j) + String.valueOf(n) + temp.substring(j);
+                result.add(newItem);
+
+            }
+            tempResult.add(String.valueOf(n) + temp);
+        }
+
+        result.addAll(tempResult);
+
+        return result;
+
     }
-	
-	public static void main(String[] args){
-		PermutationSequence ps = new PermutationSequence();
-		ps.getPermutation(4, 3);
+
+    public String getPermutation(int n, int k) {
+        ArrayList<String> list = getPermutations(n);
+        System.out.println(list.get(k - 1));
+        return list.get(k - 1);
+    }
+
+    public static void main(String[] args) {
+        PermutationSequence ps = new PermutationSequence();
+        ps.getPermutation(4, 3);
     }
 }
