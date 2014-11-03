7d96fc9fcf2526ec67b52fcbe5a9cd8c1d3254a9
rantao <tao.rance@gmail.com>
2014-11-02 23:42:27 -0800

    updated

--- leetcode/src/com/ran/leetcode/RemoveDuplicatesFromSortedArrayII.java
+++ leetcode/src/com/ran/leetcode/RemoveDuplicatesFromSortedArrayII.java
@@ -6,34 +6,33 @@ package com.ran.leetcode;
 public class RemoveDuplicatesFromSortedArrayII {
 
 
-    public int removeDuplicates(int[] A) {
+  public int removeDuplicates(int[] A) {
 
-        if (A.length < 3) {
-            return A.length;
-        }
-        int index = 1;
-        int count = 1;
-        for (int i = 1; i < A.length; i++) {
-
-            if (A[i] == A[i - 1]) {
-                count++;
-            } else {
-                count = 1;
-            }
-
-            if (count <= 2) {
-                A[index] = A[i];
-                index++;
-            }
-        }
-        return index;
-
+    if (A.length < 3) {
+      return A.length;
     }
+    int index = 1;
+    int count = 1;
+    for (int i = 1; i < A.length; i++) {
 
-    public static void main(String[] args) {
-
-        RemoveDuplicatesFromSortedArrayII ss = new RemoveDuplicatesFromSortedArrayII();
-        int[] A = {1, 1, 1, 1};
-        ss.removeDuplicates(A);
+      if (A[i] == A[i - 1]) {
+        count++;
+      } else {
+        count = 1;
+      }
+      if (count <= 2) {
+        A[index] = A[i];
+        index++;
+      }
     }
+    return index;
+
+  }
+
+  public static void main(String[] args) {
+
+    RemoveDuplicatesFromSortedArrayII ss = new RemoveDuplicatesFromSortedArrayII();
+    int[] A = {1, 1, 1, 1};
+    ss.removeDuplicates(A);
+  }
 }
--- leetcode/src/com/ran/leetcode/WordBreak.java
+++ leetcode/src/com/ran/leetcode/WordBreak.java
@@ -16,71 +16,94 @@ import java.util.Set;
 
 public class WordBreak {
 
-    public static void main(String[] args) {
+  public static void main(String[] args) {
 
-        WordBreak wb = new WordBreak();
-        String s = "abcd";
-        Set<String> dict = new HashSet<>();
-        dict.add("a");
-        dict.add("abc");
-        dict.add("b");
-        dict.add("cd");
-        System.out.println(wb.wordBreak(s, dict));
+    WordBreak wb = new WordBreak();
+    String s = "abcd";
+    Set<String> dict = new HashSet<>();
+    dict.add("a");
+    dict.add("abc");
+    dict.add("b");
+    dict.add("cd");
+    System.out.println(wb.wordBreak(s, dict));
+  }
+
+  public boolean wordBreak(String s, Set<String> dict) {
+
+    if (dict.contains(s) == true) {
+      return true;
     }
 
-    public boolean wordBreak(String s, Set<String> dict) {
+    int length = s.length();
+    for (int i = 1; i < length; i++) {
 
-        if (dict.contains(s) == true) {
-            return true;
+      String firstPart = s.substring(0, i);
+      if (dict.contains(firstPart) == true) {
+
+        String remainPart = s.substring(i, length);
+
+        boolean result = wordBreak(remainPart, dict);
+        if (result == true) {
+          return true;
         }
-
-        int length = s.length();
-        for (int i = 1; i < length; i++) {
-
-            String firstPart = s.substring(0, i);
-            if (dict.contains(firstPart) == true) {
-
-                String remainPart = s.substring(i, length);
-
-                boolean result = wordBreak(remainPart, dict);
-                if (result == true) {
-                    return true;
-                }
-            }
-        }
-
-        return false;
+      }
     }
 
-    // DP
-    public boolean wordBreak2(String s, Set<String> dict) {
+    return false;
+  }
 
-        if (s == null || s.length() == 0 || dict == null) {
-            return true;
-        }
+  // DP
+  public boolean wordBreak2(String s, Set<String> dict) {
 
-        int length = s.length();
-
-        // dp[i] is true when the substring from i to the end can be partitioned
-        boolean[] dp = new boolean[length + 1];
-        for (boolean b : dp) {
-            b = false;
-        }
-
-        // empty string can be partitioned for sure
-        dp[length] = true;
-
-        // bottom up dp, start from the end
-        for (int i = length - 1; i >= 0; i--) {
-            for (int j = i; j < length; j++) {
-                String sub = s.substring(i, j + 1);
-                if (dict.contains(sub) == true && dp[j + 1] == true) {
-                    dp[i] = true;
-                    break; // break, just jump out current level of loop
-                }
-            }
-        }
-
-        return dp[0];
+    if (s == null || s.length() == 0 || dict == null) {
+      return true;
     }
+
+    int length = s.length();
+
+    // dp[i] is true when the substring from i to the end can be partitioned
+    boolean[] dp = new boolean[length + 1];
+    for (boolean b : dp) {
+      b = false;
+    }
+
+    // empty string can be partitioned for sure
+    dp[length] = true;
+
+    // bottom up dp, start from the end
+    for (int i = length - 1; i >= 0; i--) {
+      for (int j = i; j < length; j++) {
+        String sub = s.substring(i, j + 1);
+        if (dict.contains(sub) == true && dp[j + 1] == true) {
+          dp[i] = true;
+          break; // break, just jump out current level of loop
+        }
+      }
+    }
+
+    return dp[0];
+  }
+
+  public boolean wordBreak3(String s, Set<String> dict) {
+    if (dict == null || dict.size() == 0)
+      return false;
+    if (dict.contains(s))
+      return true;
+
+    boolean res[] = new boolean[s.length() + 1];
+    res[0] = true;
+
+    for (int i = 0; i < dict.size(); i++) {
+
+      String tempStr = s.substring(0, i + 1);
+      for (int j = 0; j < i; j++) {
+        if (res[j] && dict.contains(tempStr)) {
+          res[i + 1] = true;
+          break;
+        }
+        tempStr = tempStr.substring(1, tempStr.length());
+      }
+    }
+    return res[s.length()];
+  }
 }
